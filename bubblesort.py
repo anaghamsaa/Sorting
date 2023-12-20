55e3853889c17b911b3234e7f356cbb29339ed1f
anaghamsaa <anaghamsaa@gmail.com>
2023-12-20 21:06:53 +0530

    modified bubblesort

--- bubblesort.py
+++ bubblesort.py
@@ -1,21 +1,16 @@
 #time complexity O(n^2)
 
-print (" BubbleSort using python")
-
+print (" BubbleSort using Python")
 n=int(input("Enter limit:"))
 arr = []
 for i in range(n):
     arr.append(input("Enter element"))
-
-
 for i in range(n-2):    
-    for j in range(i,n-1):
+    for j in range(n-i-1):          # there is sorted and unsorted part, where unsorted part ends at n-i-1 
         print(i,j)
         if(arr[j]>arr[j+1]):
             t=arr[j+1]
             arr[j+1]=arr[j]
-            arr[j]=t
-           
-        
+            arr[j]=t     
 for k in range(n):
     print(arr[k])
